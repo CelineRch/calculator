f05a43f4839d7d9a49efa2c80974ae1919e4d529
Celine <181670937+CelineRch@users.noreply.github.com>
2025-08-18 11:42:01 +0200

    Add Button Content
    Add content for each button

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-let container = document.querySelector(".container");
+const container = document.querySelector(".container");
+const buttonDiv = document.querySelector(".buttons");
 
 init();
 
 function init() {
-    let buttonDiv = document.querySelector(".buttons");
-    for (let i = 0; i < 4 * 5; i++) {
+    createButtons();
+}
+
+function createButtons() {
+    const labels = [
+    "", "C", "AC", "%",
+    "X", "7", "8", "9",
+    "/", "4", "5", "6",
+    "-", "1", "2", "3",
+    "+", "0", ".", "="
+];
+
+    for (let i = 1; i <= 4 * 5; i++) {
         let button = document.createElement("button");
         if (i == 0 || i == 1) { 
             button.classList.toggle("reset-buttons");
         } else {
             button.classList.toggle("normal-button");
-        }
-        button.textContent = i;
-        button.style.height = button.style.width + "px";
+        }        
+        button.id = "btn" + i;
+        button.textContent = labels[i - 1];
         buttonDiv.appendChild(button);
     }
 }
