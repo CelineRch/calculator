05f6ff4af51a3d0eed5ce4c3189a45dda8af43b7
Celine <181670937+CelineRch@users.noreply.github.com>
2025-08-20 11:47:34 +0200

    Continue calculations logic
    Solve issues regarding operators following others

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const container = document.querySelector(".container");
 const btnDiv = document.querySelector(".buttons");
 const displayArea = document.querySelector(".display-area");
+let activeOperator;
 
-let a = "";
+let a = "0";
 let b = "";
 let operator = "";
 let isFirstTerm = true;
+let isResult = false;
 
 init();
 
@@ -35,7 +37,9 @@ function buttonClicked(btn) {
             selectOperator(btn);
             break;
         case "equal-button":
-            updateDisplay(operate(a, b, operator));
+            manageOperation();
+        case "komma-button":
+            addKomma(btn);
             break;
         default:
             break;
@@ -49,10 +53,33 @@ function updateDisplay(content) {
 }
 
 function reset() {
-    a = "";
+    a = "0";
     b = "";
     operator = "";
     isFirstTerm = true;
+}
+
+function addNumber(val) {
+    if (isFirstTerm) {
+        if (isResult) {
+            a = val.textContent;
+            isResult = false;
+        } else {
+            a += val.textContent;
+        }
+        updateDisplay(a);
+    } else {
+        b += val.textContent;
+        updateDisplay(b);
+    }
+}
+
+function addKomma(val) {
+    if (isFirstTerm && !a.includes(".")) {
+        addNumber(val);
+    } else if (!isFirstTerm && !b.includes(".")) {
+        addNumber(val);
+    }
 }
 
 function deleteNumber() {
@@ -65,24 +92,32 @@ function deleteNumber() {
     }
 }
 
-function addNumber(val) {
-    if (isFirstTerm) {
-        a += val.textContent;
-        updateDisplay(a);
-    } else {
-        b += val.textContent;
-        updateDisplay(b);
-    }
-}
 
 function selectOperator(choice) {
+    if (activeOperator) activeOperator.classList.toggle("active-operator", false);
+    activeOperator = choice;
+    activeOperator.classList.toggle("active-operator", true);
+
     if (!isFirstTerm && b != "") {
-        let result = operate(a, b, operator);
-        a = result;
+        if (!operator) operator = choice.textContent;
+        a = operate(a, b, operator);
+        b = "";
     }
     operator = choice.textContent;
     isFirstTerm = false;
     updateDisplay(a+operator);
+}
+
+function manageOperation() {
+    if (activeOperator) activeOperator.classList.toggle("active-operator", false);
+    if (b != "") {
+        a = operate(a, b, operator);
+        b = "";
+        operator = "";
+    }
+    isFirstTerm = true;
+    isResult = true;
+    updateDisplay(a);
 }
 
 function operate(a, b, operator) {
@@ -104,8 +139,7 @@ function operate(a, b, operator) {
             result = divide(a, 100);
             break;
     }
-    reset();
-    return result;
+    return Math.round(result * 10000) / 10000;
 }
 
 function add(a, b) {
